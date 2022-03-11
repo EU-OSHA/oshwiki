882046caeea724091e576196f8b8b335873bbbce
Laura Spearey Luengo <lspearey@bilbomatica.es>
2022-03-11 12:45:48 +0100

    first commit subtheme

---
+++ themes/custom/oshwiki_theme/js/global.js
@@ -0,0 +1,20 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.allages_theme = {
+    attach: function (context, settings) {
+
+
+    }
+  };
+
+
+})(jQuery, Drupal);
+
+
